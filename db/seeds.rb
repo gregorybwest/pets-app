9bdc3f58500d9d12969c716641bbd722152e3b4e
greg <gregorybwest@gmail.com>
2022-09-15 11:43:12 -0700

    pets index action and view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Pet.create(name: "Romeo", breed: "German Shepard", image: "https://www.akc.org/wp-content/uploads/2017/11/German-Shepherd-on-White-00.jpg")
+Pet.create(name: "Moose", breed: "French Bulldog", image: "https://frenchiejourney.com/wp-content/uploads/2021/12/Blue-French-Bulldog.png")
+
