5d1e90b3d8db68cf23317868052eaaa632990485
greg <gregorybwest@gmail.com>
2022-09-16 15:16:11 -0700

    crud actions except for delete

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -3,4 +3,39 @@ class PetsController < ApplicationController
     @pets = Pet.all
     render template: "pets/index"
   end
+
+  def show
+    @pet = Pet.find_by(id: params[:id])
+    render template: "pets/show"
+  end
+
+  def new
+    @pet = Pet.new
+    render template: "pets/new"
+  end
+
+  def create
+    @pet = Pet.new(
+      name: params[:pet][:name],
+      breed: params[:pet][:breed],
+      image: params[:pet][:image],
+    )
+    @pet.save
+    redirect_to "/pets"
+  end
+
+  def edit
+    @pet = Pet.find_by(id: params[:id])
+    render template: "pets/edit"
+  end
+
+  def update
+    @pet = Pet.find_by(id: params[:id])
+    @pet.name = params[:pet][:name]
+    @pet.breed = params[:pet][:breed]
+    @pet.image = params[:pet][:image]
+    @pet.save
+    redirect_to "/pets"
+  end
+
 end
